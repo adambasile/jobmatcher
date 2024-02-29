f7849f328737ba2842c01c668fb8be3b7060bad2
Adam Basile <basile.adam@gmail.com>
2024-02-29 15:10:55 +1100

    Split things out into functions

--- src/jobmatcher/matcher.py
+++ src/jobmatcher/matcher.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
-from os import PathLike
-from typing import Any
+from pathlib import Path
+from typing import Tuple, TypeAlias
 
 import polars as pl
 
+Pathlike: TypeAlias = str | Path
 
-def match_jobs_from_csv(jobseekers_csv: PathLike[Any], jobs_csv: PathLike[Any]) -> pl.DataFrame:
+
+def match_jobs_from_csv(jobseekers_csv: Pathlike, jobs_csv: Pathlike) -> pl.DataFrame:
     """
     :param jobseekers_csv:
         path to csv with three columns: ["id", "name", "skills"], where skills is a comma separated list
@@ -22,37 +24,37 @@ def match_jobs_from_csv(jobseekers_csv: PathLike[Any], jobs_csv: PathLike[Any]) 
 
 
 def match_jobs_from_frames(jobseekers: pl.DataFrame, jobs: pl.DataFrame) -> pl.DataFrame:
-    jobs, jobseekers = reformat_inputs(jobseekers, jobs)
+    jobseekers, jobs = preprocess_inputs(jobseekers, jobs)
+    job_matches = match_to_jobs(jobseekers, jobs)
+    return format_for_output(job_matches, jobseekers, jobs)
 
+
+def format_for_output(job_matches: pl.DataFrame, jobseekers: pl.DataFrame, jobs: pl.DataFrame) -> pl.DataFrame:
+    messy_output = job_matches.join(jobs, on="job_id").join(jobseekers, on="jobseeker_id")
+    pretty_formatted = messy_output.select(
+        pl.col(
+            "jobseeker_id", "jobseeker_name", "job_id", "job_title", "matching_skill_count", "matching_skill_percent"
+        )
+    ).sort(
+        [pl.col("jobseeker_id"), pl.col("matching_skill_percent"), pl.col("job_id")],
+        descending=[False, True, False],
+    )
+    return pretty_formatted
+
+
+def match_to_jobs(jobseekers: pl.DataFrame, jobs: pl.DataFrame) -> pl.DataFrame:
     num_matched_skills = (
         jobseekers.explode("skills")
         .join(jobs.explode("required_skills"), left_on="skills", right_on="required_skills")
         .group_by([pl.col("jobseeker_id", "job_id")])
         .agg(pl.len().alias("matching_skill_count"))
-    )
-
-    return (
-        num_matched_skills.join(jobs, on="job_id")
-        .join(jobseekers, on="jobseeker_id")
+        .join(jobs.select(pl.col("job_id", "num_skills")), on="job_id")
         .with_columns(((100 * pl.col("matching_skill_count")) / pl.col("num_skills")).alias("matching_skill_percent"))
-        .select(
-            pl.col(
-                "jobseeker_id",
-                "jobseeker_name",
-                "job_id",
-                "job_title",
-                "matching_skill_count",
-                "matching_skill_percent",
-            )
-        )
-        .sort(
-            [pl.col("jobseeker_id"), pl.col("matching_skill_percent"), pl.col("job_id")],
-            descending=[False, True, False],
-        )
     )
+    return num_matched_skills
 
 
-def reformat_inputs(jobseekers: pl.DataFrame, jobs: pl.DataFrame):
+def preprocess_inputs(jobseekers: pl.DataFrame, jobs: pl.DataFrame) -> Tuple[pl.DataFrame, pl.DataFrame]:
     """
     :param jobseekers:
         pl.DataFrame with columns: ["id", "name", "skills"] where skills is a string containing a comma separated list
@@ -68,10 +70,13 @@ def reformat_inputs(jobseekers: pl.DataFrame, jobs: pl.DataFrame):
     jobseekers = jobseekers.rename({col: f"jobseeker_{col}" for col in ["id", "name"]})
     jobs = jobs.rename({col: f"job_{col}" for col in ["id", "title"]})
     # split the skills
-    # this splits on commas and then strips whitespace from each element
-    split = lambda col: pl.col(col).str.split(",").list.eval(pl.element().str.strip_chars())
-    jobseekers = jobseekers.with_columns(split("skills"))
-    jobs = jobs.with_columns(split("required_skills")).with_columns(
+    jobseekers = jobseekers.with_columns(split_and_strip("skills"))
+    jobs = jobs.with_columns(split_and_strip("required_skills")).with_columns(
         pl.col("required_skills").list.len().alias("num_skills")
     )
-    return jobs, jobseekers
+    return jobseekers, jobs
+
+
+def split_and_strip(col: str) -> pl.Expr:
+    # this splits on commas and then strips whitespace from each element
+    return pl.col(col).str.split(",").list.eval(pl.element().str.strip_chars())
